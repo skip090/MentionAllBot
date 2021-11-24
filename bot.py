a4f570f4b1a6550e38a8bbe04aadcace70c3a66c
skip090 <93792897+skip090@users.noreply.github.com>
2021-11-25 01:27:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ async def start(event):
 
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@koliXsupport](https://t.me/koliXsupport) on Telegram"
+  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nOwner [@loltage](https://t.me/loltage) on Telegram"
   await event.reply(
     helptext,
     link_preview=False,
